003496e7634b1f1dc4f9375bad84b1694c4c12c8
mattshade <matt.shade@nbcuni.com>
2020-11-12 17:46:09 -0500

    adds proper 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+interface Props {
+    data: ''
+    location: Location
+  }
+
+const FOURoFOUR: React.FC<Props> = ({ data, location }: Props) => {
+    return (
+        <div>it's not here!</div>
+    )
+}
+export default FOURoFOUR
